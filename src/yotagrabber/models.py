fa2f41a273e8b5108855a451d18b088e8a3ddfa6
Greg Gemmer <ghgemmer@gmail.com>
2025-03-26 14:40:45 -0500

    Added prius prime to models to get

--- src/yotagrabber/models.py
+++ src/yotagrabber/models.py
@@ -114,6 +114,8 @@ def update_models():
     models.drop_duplicates(subset=["modelCode"], inplace=True)
     new_model_row2 = pd.DataFrame({'modelCode': ['4runnerhybrid'], 'title': ['4Runner Hybrid']})
     models = pd.concat([models, new_model_row2], ignore_index=True)
+    new_model_row3 = pd.DataFrame({'modelCode': ['priusprime'], 'title': ['Prius Prime']})
+    models = pd.concat([models, new_model_row3], ignore_index=True)
     models.drop_duplicates(subset=["modelCode"], inplace=True)
     
     # Toyota uses different names for some models when you query the graphQL API.
